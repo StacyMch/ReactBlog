6da537eed24179e98f5b5f551ef963828321b471
StacyMch <greensummertea@gmail.com>
2025-03-15 01:09:59 +0300

    0

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<div>123</div>
+		</Div>
+	);
+};
