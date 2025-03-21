3431c3979091d941ab471f4f703a9898a7324f18
StacyMch <greensummertea@gmail.com>
2025-03-21 21:13:02 +0300

    Реализовать шапку

--- my-app/src/blog.js
+++ my-app/src/blog.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
 
-const Header = () => <div>Шапка</div>;
 const Footer = () => <div>Футер</div>;
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100vh;
+	width: 1000px;
+	min-height: 100%;
+	background-color: #fff;
+	margin: 0 auto;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
@@ -14,7 +25,7 @@ const H2 = styled.h2`
 `;
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -29,6 +40,6 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
