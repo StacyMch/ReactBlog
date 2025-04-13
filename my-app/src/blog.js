80ee29b2d073fd1f98ebe893e0a0e5be184d362a
StacyMch <greensummertea@gmail.com>
2025-04-14 00:47:49 +0300

    Реализовать поиск на Главной - доработки с версткой пагинации

--- my-app/src/blog.js
+++ my-app/src/blog.js
@@ -8,10 +8,11 @@ import { Authorization, Main, Post, Registration, Users } from './pages';
 import styled from 'styled-components';
 
 const AppColumn = styled.div`
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-	height: 100vh;
+
 	width: 1000px;
 	min-height: 100%;
 	background-color: #fff;
